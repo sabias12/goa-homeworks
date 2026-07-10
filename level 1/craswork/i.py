22484c07e3df1f1f5daa640caea8757e31805b80
sabias12 <sabasabuka8888@gmail.com>
2026-07-10 14:30:23 +0400

    commit

---
+++ level 1/craswork/i.py
@@ -0,0 +1,11 @@
+name = input("შეიყვანე შენი სახელი")
+
+print("heelo", name)
+
+name = input("შეიყვანე შენი სახელი: ")
+surname = input("შეიყვანე შენი გვარი: " )
+age = input("შეიყვანე ასაკი: ")
+
+print("სახელი:", name)
+print("გვარი:", surname)
+print("ასაკი:", age)
