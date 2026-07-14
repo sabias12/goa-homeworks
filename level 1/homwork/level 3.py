a9c181f5e231278a21d777d691a1d77c5a3d92ae
sabias12 <sabasabuka8888@gmail.com>
2026-07-14 18:18:17 +0400

    commit

---
+++ level 1/homwork/level 3.py
@@ -0,0 +1,28 @@
+# input იღებს მონაცემებს მომხმარებლისგან 
+#print() გამოაქვს ინფორმაცია ეკრანზე 
+#int()ტექსტს მთლიან რიცხვად გადააქცევს 
+#float() ტექსტს მთლიან რიცხვად გაგააქცევს
+
+name = input("ეიყვანეთ თქვენი სახელი")
+age = input("შეიყვანეთ თქვენი ასაკი")
+username = input("შეიყვანე თქვენი  გვარი")
+color = input("შეიყვანეთ ფერი")
+citi = input("საყვარელი ქალაქი")
+
+print(name)
+print(age)
+print(username)
+print(color)
+print(citi)
+
+name = input("შეიყვანე სახელი")
+print("შენი სახელია name:,")
+
+age = int(input("შეიყვანე ასაკი"))
+print("შენი ასაკია age")
+city = input("შეიყვანე ქალაქი")
+print("შენ ცხოვრობ city")
+color = input("შეიყვანე შენი საყვარელი ფერი")
+print("შენი საყაველი ფერია color")
+
+
---
+++ level 1/homwork/level 4.py
@@ -0,0 +1,23 @@
+name = input("შეიყავანე სახელი")
+age =  ("შეიყვანე ასაკი")
+username = input("შეიყვანე გვარი")
+
+print(name)
+print(age)
+print(usarname)
+
+num1 = float(input("შეიყვანე პირველი ათწილადი რიცხვი"))
+num2 = float(input("შეიყვანე მეორე ათწილადი რიცხვი"))
+
+print("ჯამი:", num1 + num2)
+print("სხვაობა:", num1 - num2)
+print("ნამრავლი:", num1 * num2)
+print("განაყოფი:", num1 / num2)
+
+name = type(input("სახელი"))
+age =  int (input("ასაკი"))
+ricxvi = float (input("საყვარელი რიცხვი"))
+
+print(name)
+print(age)
+print(ricxvi)
