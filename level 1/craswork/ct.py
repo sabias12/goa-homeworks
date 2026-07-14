ca74815cbd9f00fdf86aea4cf9d7ae387e800e3a
sabias12 <sabasabuka8888@gmail.com>
2026-07-14 14:33:25 +0400

    commint

---
+++ level 1/craswork/ct.py
@@ -0,0 +1,11 @@
+num = int(input("შეივანე მთელი რიცხვი: "))
+
+num2 = float(input("შევიყვანოთ ათწილადი რიცხვი"))
+
+num3 = str(input("შეიყვანე ტექსტი"))
+
+print(type(num))
+
+print(type(num2))
+
+print(type(num3))
---
+++ level 1/craswork/cu.py
@@ -0,0 +1,9 @@
+num1 = int(input("შეიყვანე შენი სახელი"))
+num2 = int(input("შეიყვანე შენი გვარი"))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 > num2)
