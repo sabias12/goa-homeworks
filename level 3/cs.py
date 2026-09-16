ac41171d969ea6dbbb5ba646919a405774ad033a
sabias12 <sabasabuka8888@gmail.com>
2026-09-16 18:18:10 +0400

    Commit

---
+++ level 3/cs.py
@@ -0,0 +1,77 @@
+# name = input("შეიყვანე სახელი")
+
+# print("გამარჯობა",name)
+
+# num1 = int(input("შეიყვანე რიცხვი"))
+# num2 =int(input("შეიყვანე რიცხვი"))
+
+# print(num1 + num2)
+
+# num = int(input("შეიყვანე ასაკი"))
+
+# if num >=18:
+#     print("სწულწლოვანი ხარ")
+# else:
+#     print("არასრულწლოვანი ხარ")
+
+
+# xili = ["ვაშლი","ბანანი","ატამი"]
+
+# print(len(xili))
+
+# num = int(input('შეიყვანე რიცხვი'))
+
+# for i in range(1,num):
+#     print(i)
+#     i = i+1
+
+# num =[33,22,44]
+
+# num.append(34)
+
+# print(num)
+
+# num1 = int(input("შეიყვანე რიცხვი"))
+
+# num2 =int(input("შეიყვანე რიხვი"))
+
+# if num1 >= num2:
+#     print("num1 უფრო მეტია")
+# else:
+#     print("num2 უფრო მეტია")
+
+
+# xili = ['ვაშლი','ბანანი','ატამი']
+
+# xili.append('მარწყვი')
+
+# print(xili)
+
+
+# num = int(input("შეიყვანე რიცხვი"))
+
+# if num >=0:
+#     print("დადებითია")
+# else:
+#     print('არ არის დადებითი')
+
+
+# num = [33,55,22,55,]
+
+# print(len(num))
+
+
+# for i in range(2,10,2):
+#     print(i)
+
+
+# color = ['blue','red','green']
+
+# print("blue" in color)
+
+
+num = int(input("შეიყვანე ასაკი"))
+
+if num <13:
+    print("ბავშვია")
+    
